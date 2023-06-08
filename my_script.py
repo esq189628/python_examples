cf54f648044f8570e022863105661ffe91ada413
Inoris Esquea <inoris@gmail.com>
2023-06-08 04:46:41 +0000

    Add my.script.py

---
+++ my_script.py
@@ -0,0 +1,7 @@
+def main():
+    strings = ["Hello", "world", "I", "am", "Python"]
+    for string in strings:
+        print("This is a string: " + string)
+
+if __name__ == "__main__":
+    main()
